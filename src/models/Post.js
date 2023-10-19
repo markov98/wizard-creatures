677f8f2f3c0f9957cd6385d1f7f5f8511de9fb5f
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-19 23:00:31 +0300

    added post model

---
+++ src/models/Post.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [2, 'Name must be at least 2 characters long!']
+    },
+    species: {
+        type: String,
+        required: [true, 'Species is required!'],
+        minLength: [3, 'Spicies must be at least 3 characters long!']
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image URL is required!'],
+        match: [/^https?:\/\//, 'Invalid image URL!']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+        minLength: [5, 'Description must be between 5 and 50 characters long!'],
+        maxLength: [500, 'Description must be between 5 and 500 characters long!']
+    },
+    skinColor: {
+        type: String,
+        required: [true, 'Skin Color is required!'],
+        minLength: [3, 'Skin Color must be at least 3 characters long!']
+    },
+    eyeColor: {
+        type: String,
+        required: [true, 'Eye Color is required!'],
+        minLength: [3, 'Eye Color must be at least 3 characters long!']
+    },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    voters: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }]
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
